a258cab8da81092dfb5039799599b0120fdc392d
dancal <dancal@empas.com>
2022-04-14 18:51:40 +0900

    -

--- src/rqt_virtual_joy/virtual_joy_module.py
+++ src/rqt_virtual_joy/virtual_joy_module.py
@@ -9,7 +9,10 @@ from python_qt_binding.QtWidgets import QWidget,QGraphicsView
 from python_qt_binding.QtGui import QCursor
 from python_qt_binding import QtCore
 
+from math import fabs
+from numpy import array_equal
 
+# rosrun rqt_virtual_joy rqt_virtual_joy
 class MyPlugin(Plugin):
 
     def __init__(self, context):
@@ -48,9 +51,21 @@ class MyPlugin(Plugin):
             print('arguments: ', args)
             print('unknowns: ', unknowns)
 
+        # target
         self.target_joy = Joy()
         self.target_joy.axes = [0.,0.,1.,0.,0.,1.,0.,0.]
         self.target_joy.buttons = [0,0,0,0,0,0,0,0,0,0,0]
+
+        # last
+        self.last_joy = Joy()
+        self.last_joy.axes = [0.,0.,1.,0.,0.,1.,0.,0.]
+        self.last_joy.buttons = [0,0,0,0,0,0,0,0,0,0,0]
+        self.last_send_time = rospy.Time.now()
+
+        self.use_button = True
+
+        self.speed_index = 2
+        self.available_speeds = [0.5, 1.0, 3.0, 4.0]
 
         # Create QWidget
         self._widget = QWidget()
@@ -126,11 +141,10 @@ class MyPlugin(Plugin):
 
         if self._widget.publishCheckBox.checkState() == QtCore.Qt.Checked:
             rate = self._widget.rateSpinBox.value()
-            self.startIntervalTimer(float(1000.0/rate))
+            self.startIntervalTimer(float(10000.0/rate))
         else:
             self.startIntervalTimer(-1)  # Stop Timer (Stop Publish)
 
-    
     def indexChanged(self,index):
         text = str(self._widget.shapeSelectBox.currentText())
         self._widget.joy.setMode(str(text))
@@ -146,32 +160,53 @@ class MyPlugin(Plugin):
         text = "({:1.2f},{:1.2f})".format(pos['x'],pos['y'])
         self._widget.joyPosLabel.setText(text)
 
+    def ramped_vel(self,v_prev,v_target,t_prev,t_now):
+        # This function was originally not written by me:
+        # https://github.com/osrf/rosbook/blob/master/teleop_bot/keys_to_twist_with_ramps.py
+        step = (t_now - t_prev).to_sec()
+        sign = self.available_speeds[self.speed_index] if \
+                (v_target > v_prev) else -self.available_speeds[self.speed_index]
+        error = fabs(v_target - v_prev)
+
+        # if we can get there within this timestep -> we're done.
+        if error < self.available_speeds[self.speed_index]*step:
+            return v_target
+        else:
+            return v_prev + sign * step # take a step toward the target
+
     def processTimerShot(self):
-        joy = self.getROSJoyValue()
+        t_now           = rospy.Time.now()
+
+        joy             = self.getROSJoyValue()
 
         msg             = Joy()
         msg.axes        = self.target_joy.axes
-        #msg.buttons     = self.target_joy.buttons
+        msg.buttons     = self.last_joy.buttons
 
         msg.header.stamp = rospy.Time.now()
         msg.axes[0]     = float(joy['x'])
         msg.axes[1]     = float(joy['y'])
+        #msg.axes[3]     = float(joy['x'])
+        #msg.axes[4]     = float(joy['y'])
         #msg.axes.append(float(joy['x']))
         #msg.axes.append(float(joy['y']))
+        # joy.axes.append(self.ramped_vel(self.last_joy.axes[i], self.target_joy.axes[i],self.last_send_time,t_now))
 
-        button_num = 1
-        while True:
-            try:
-                msg.buttons.append(eval("self._widget.button"+str(button_num)).isDown())
-                button_num+=1
-            except:
-                break
+        for i in range(len(self.target_joy.axes)):
+            i = i + 1
+            msg.buttons[i] = int(eval("self._widget.button"+str(i)).isDown() == True)
+
+        #msg.buttons[2]  = True
+        self.last_joy.buttons = msg.buttons
+        #print("buttons_change = ", msg.buttons)
 
         try:
             self.pub.publish(msg)
         except:
             rospy.logwarn("publisher not initialized")
             pass
+
+        self.last_send_time = t_now
 
     def getROSJoyValue(self):
         return self._widget.joy.getJoyValue()
